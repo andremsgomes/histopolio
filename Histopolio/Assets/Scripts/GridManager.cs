3e1f2b8706c41eea41d5fa7e4fac4d7c66925709
andremsgomes <andremsgomes@gmail.com>
2022-02-04 01:51:27 +0000

    Added corner tiles to grid

--- Histopolio/Assets/Scripts/GridManager.cs
+++ Histopolio/Assets/Scripts/GridManager.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    [SerializeField] private Tile tilePrefab;
+    [SerializeField] private Tile groupPropertyTilePrefab;
+    [SerializeField] private Tile cornerTilePrefab;
     [SerializeField] private Camera camera;
 
     // Start is called before the first frame update
@@ -15,26 +16,44 @@ public class GridManager : MonoBehaviour
 
     // Generate all tiles
     void GenerateGrid() {
+        // Bottom tiles
         for (int i = 0; i < 8; i++) {
-            var spawnedTile = Instantiate(tilePrefab, new Vector3(i, 0), Quaternion.Euler(0,0,0));
-            spawnedTile.name = $"Tile {i} 0";
+            var spawnedTile = Instantiate(groupPropertyTilePrefab, new Vector3(i, 0), Quaternion.Euler(0,0,0));
+            spawnedTile.name = $"Tile {i+1} 0";
         }
 
+        // Top tiles
         for (int i = 0; i < 8; i++) {
-            var spawnedTile = Instantiate(tilePrefab, new Vector3(i, 9.6f), Quaternion.Euler(0,0,180));
-            spawnedTile.name = $"Tile {i} 8";
+            var spawnedTile = Instantiate(groupPropertyTilePrefab, new Vector3(i, 9.6f), Quaternion.Euler(0,0,180));
+            spawnedTile.name = $"Tile {i+1} 9";
         }
 
+        // Left tiles
         for (int i = 0; i < 8; i++) {
-            var spawnedTile = Instantiate(tilePrefab, new Vector3(-1.3f, i+1.3f), Quaternion.Euler(0,0,-90));
-            spawnedTile.name = $"Tile 0 {i}";
+            var spawnedTile = Instantiate(groupPropertyTilePrefab, new Vector3(-1.3f, i+1.3f), Quaternion.Euler(0,0,-90));
+            spawnedTile.name = $"Tile 0 {i+1}";
         }
 
+        // Right tiles
         for (int i = 0; i < 8; i++) {
-            var spawnedTile = Instantiate(tilePrefab, new Vector3(8.3f, i+1.3f), Quaternion.Euler(0,0,90));
-            spawnedTile.name = $"Tile 8 {i}";
+            var spawnedTile = Instantiate(groupPropertyTilePrefab, new Vector3(8.3f, i+1.3f), Quaternion.Euler(0,0,90));
+            spawnedTile.name = $"Tile 9 {i+1}";
         }
 
+        // Corners
+        var spawnedCornerTile1 = Instantiate(cornerTilePrefab, new Vector3(-1.3f, 0), Quaternion.Euler(0,0,0));
+        spawnedCornerTile1.name = $"Tile 0 0";
+
+        var spawnedCornerTile2 = Instantiate(cornerTilePrefab, new Vector3(-1.3f, 9.6f), Quaternion.Euler(0,0,-90));
+        spawnedCornerTile2.name = $"Tile 0 9";
+
+        var spawnedCornerTile3 = Instantiate(cornerTilePrefab, new Vector3(8.3f, 9.6f), Quaternion.Euler(0,0,180));
+        spawnedCornerTile3.name = $"Tile 9 9";
+
+        var spawnedCornerTile4 = Instantiate(cornerTilePrefab, new Vector3(8.3f, 0), Quaternion.Euler(0,0,90));
+        spawnedCornerTile4.name = $"Tile 9 0";
+
+        // Change camera settings
         camera.transform.position = new Vector3(3.5f, 4.8f, -10);
         camera.orthographicSize = 6;
     }
