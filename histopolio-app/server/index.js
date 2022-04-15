d7b4b5f9e18daec318dda377293bfbb2621fe29b
andremsgomes <andremsgomes@gmail.com>
2022-04-15 14:59:50 +0100

    Added mongo db

--- histopolio-app/server/index.js
+++ histopolio-app/server/index.js
@@ -2,12 +2,15 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const apiRouter = require('./routes/api-router')
+const db = require('./db')
 
 const app = express()
 const apiPort = 5000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
