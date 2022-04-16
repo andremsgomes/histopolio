8d85059daf808430b821776507f60adf505c3bb0
andremsgomes <andremsgomes@gmail.com>
2022-04-16 21:15:08 +0100

    Added websocket

--- histopolio-app/server/index.js
+++ histopolio-app/server/index.js
@@ -1,16 +1,33 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const http = require('http');
+
+const WebSocket = require('ws');
 
 const apiRouter = require('./routes/api-router')
-const db = require('./db')
+// const db = require('./db')
 
 const app = express()
-const apiPort = 5000
+const apiPort = 8080
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server: server }, ()=>{
+    console.log('server started')
+});
+
+wss.on('connection', function connection(ws) {
+    console.log('A new client connected!')
+    ws.send('Welcome New Client')
+
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+    });
+});
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -18,4 +35,4 @@ app.get('/', (req, res) => {
 
 app.use('/api', apiRouter)
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+server.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
