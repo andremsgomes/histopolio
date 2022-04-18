03310190e6b541249bf073052673aeb3c35d1636
andremsgomes <andremsgomes@gmail.com>
2022-04-18 22:06:11 +0100

    Connected frontend with server

--- histopolio-app/client/src/App.js
+++ histopolio-app/client/src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { w3cwebsocket } from 'websocket';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const client = new w3cwebsocket('ws://localhost:8080');
+
+class App extends Component {
+  componentDidMount() {
+    client.onopen = () => {
+      console.log('WebSocket Client Connected');
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
