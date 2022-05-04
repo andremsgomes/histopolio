5ac98b873a3b2e286a82041fc402624fc5d44e1e
andremsgomes <andremsgomes@gmail.com>
2022-05-05 00:38:01 +0100

    secure passwords

--- histopolio-app/server/controllers/auth-ctrl.js
+++ histopolio-app/server/controllers/auth-ctrl.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+
 const { readJSONFile, writeJSONFile } = require("./../utils/json-utils");
 
 function getUser(email) {
@@ -9,15 +11,25 @@ function getUser(email) {
 function signup(req, res) {
   const { name, email, password } = req.body;
 
+  if (!(name && email && password)) {
+    return res.status(400).send({ error: true, message: "Dados mal formatados" });
+  }
+
   if (getUser(email)) {
     return res
       .status(409)
       .json({ error: true, message: "Utilizador já existente" });
   }
 
+  // generate salt to hash password
+  const salt = bcrypt.genSaltSync(10);
+
+  // hash password
+  const hashedPassword = bcrypt.hashSync(password, salt);
+
   const credentials = {
     email: email,
-    password: password,
+    password: hashedPassword,
   };
 
   const users = readJSONFile("./data/Users.json");
@@ -33,11 +45,22 @@ function signup(req, res) {
   users.push(user);
   writeJSONFile("./data/Users.json", users);
 
-  return res.status(201).json(user);
+  const returnUser = {
+    id: user.id,
+    name: user.name,
+    points: user.points
+  }
+
+  return res.status(201).json(returnUser);
 }
 
 function login(req, res) {
   const { email, password } = req.body;
+
+  if (!(email && password)) {
+    return res.status(400).send({ error: true, message: "Dados mal formatados" });
+  }
+
   const user = getUser(email);
 
   if (!user) {
@@ -46,11 +69,17 @@ function login(req, res) {
       .json({ error: true, message: "Utilizador não encontrado" });
   }
 
-  if (user.credentials.password !== password) {
+  if (!bcrypt.compareSync(password, user.credentials.password)) {
     return res.status(403).json({ error: true, message: "Password errada" });
   }
 
-  return res.status(200).json(user);
+  const returnUser = {
+    id: user.id,
+    name: user.name,
+    points: user.points
+  }
+
+  return res.status(200).json(returnUser);
 }
 
 module.exports = {
