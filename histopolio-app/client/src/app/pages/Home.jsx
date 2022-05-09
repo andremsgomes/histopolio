e47146cdf93823165799c1c86d6752a4602c72dc
andremsgomes <andremsgomes@gmail.com>
2022-05-09 02:16:57 +0100

    Improved home page

--- histopolio-app/client/src/app/pages/Home.jsx
+++ histopolio-app/client/src/app/pages/Home.jsx
@@ -7,11 +7,14 @@ function Home() {
   const { user } = useAuth();
 
   return (
-    <div>
+    <div className="text-center">
       <h1>Olá {user.name}</h1>
       <Link to="/play" style={{ textDecoration: "none" }}>
-        <button className="btn btn-primary btn-lg">Jogar</button>
+        <button className="btn btn-primary btn-lg mt-4">
+          Jogar Histopólio
+        </button>
       </Link>
+      <h3 className="mt-3">Os teus pontos: {user.game.points}</h3>
     </div>
   );
 }
