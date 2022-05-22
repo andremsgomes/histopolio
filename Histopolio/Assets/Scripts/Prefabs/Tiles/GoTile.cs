9c92cb3398fe25a6b55b1e37837d57c1e4aebe55
andremsgomes <andremsgomes@gmail.com>
2022-05-22 21:28:43 +0100

    Go tile arrengment <3

--- Histopolio/Assets/Scripts/Prefabs/Tiles/GoTile.cs
+++ Histopolio/Assets/Scripts/Prefabs/Tiles/GoTile.cs
@@ -7,13 +7,13 @@ public class GoTile : CardTile
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     // Resize players to fit tile
@@ -28,10 +28,21 @@ public class GoTile : CardTile
             playersList[0].SetScale(new Vector3(1, 1, 1));
             playersList[0].SetPosition(new Vector3(transform.position.x, transform.position.y, -3));
         }
+        else if (playersList.Count < 3)
+        {
+            Vector3 scale = new Vector3(0.8f, 0.8f, 1);
+
+            playersList[0].SetScale(scale);
+            playersList[1].SetScale(scale);
+
+            playersList[0].SetPosition(new Vector3(transform.position.x - 0.35f, transform.position.y - 0.35f, -3));
+            playersList[1].SetPosition(new Vector3(transform.position.x + 0.35f, transform.position.y + 0.35f, -3));
+        }
     }
 
     // Get rotation for the camera
-    public override Quaternion GetCameraRotation() {
+    public override Quaternion GetCameraRotation()
+    {
         return Quaternion.Euler(0, 0, 45);
     }
 }
