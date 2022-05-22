c27c87cd596613afea281424dbfc1ccd77f70418
andremsgomes <andremsgomes@gmail.com>
2022-05-22 16:47:48 +0100

    go to prison perform action

--- Histopolio/Assets/Scripts/Prefabs/Tiles/GoToPrisonTile.cs
+++ Histopolio/Assets/Scripts/Prefabs/Tiles/GoToPrisonTile.cs
@@ -16,6 +16,11 @@ public class GoToPrisonTile : CardTile
         
     }
 
+    // Perform action when player arrives to tile
+    public override void PerformAction() {
+        gameController.MovePlayerTo(10);
+    }
+
     // Get rotation for the tile
     public override Quaternion GetCameraRotation() {
         return Quaternion.Euler(0, 0, 135);
